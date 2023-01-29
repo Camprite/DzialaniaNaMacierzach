06fdb8f167c5bbeb211eaa1dafc97b80d6423405
Camprite <u19_dawidborkowski1@zsp1.siedlce.pl>
2023-01-29 21:38:09 +0100

    niewiemocb

--- DzialaniaNaMacierzach/DzialaniaNaMacierzach.cpp
+++ DzialaniaNaMacierzach/DzialaniaNaMacierzach.cpp
@@ -2,56 +2,44 @@
 #include <locale.h>
 #include <fstream>
 #include "funkcje.h"
-
+#include <time.h>
+#include <cstdlib>
 using namespace std;
-
-int*** matrixarray;//tablica dynamiczna z macierzami
-int** matrixspec ;//tablica przechowująca dane poszczególnej macierzy
-int* matrixnumber = new int;//ilosć macierzy
 int main()
 {
-    //system("color 0a"); // alternatywna wersja kolorków
-    system("color f0"); //zmiana koloru konsoli :)
-    setlocale(LC_CTYPE, "Polish"); //polskie znaczki 
-
-  
-        switch (startMenu())
-    {
-    case 1: { //---------DANE-------Z--------PLIKU-----------
-        string plik;
-       enterMartixNumberFromFile(plik, matrixarray, matrixspec, matrixnumber);
-        break;
-    }
-    case 2: {//---------DANE--------Z--------KLAIWATURY-----------
-        enterMartixNumberFromKeyboard(matrixarray, matrixspec, matrixnumber);
-
-        break;
-    }
-    case 3: {//---------DANE--------PSEUDOLOSOWE------------
-       enterMartixNumberFromRandom(matrixarray, matrixspec, matrixnumber);
-        cout << "Udało się";
-        break;
-    }
-    default:
-        return 0;
-    }
-        cout << "Podaj dalsze działanie" << endl;
-
-
-
-    return 1;
+	int*** matrixarray = new int** [50];
+	int** matrixspec = new int* [50];
+	int* matrixnumber = new int[50];//ilosć macierzy
+	for (int i = 0;i < 50;i++) {
+		matrixarray[i] = new int* [50];
+		matrixspec[i] = new int[50];
+		for (int j = 0;j < 50;j++) {
+			matrixarray[i][j] = new int[50];
+		}
+	}
+	srand(time(NULL));
+	//system("color 0a"); // alternatywna wersja kolorków
+	system("color f0"); //zmiana koloru konsoli :)
+	setlocale(LC_CTYPE, "Polish"); //polskie znaczki 
+	switch (startMenu())
+	{
+	case 1: { //---------DANE-------Z--------PLIKU-----------
+		string plik;
+		enterMartixNumberFromFile(plik, matrixarray, matrixspec, matrixnumber);
+		break;
+	}
+	case 2: {//---------DANE--------Z--------KLAIWATURY-----------
+		enterMartixNumberFromKeyboard(matrixarray, matrixspec, matrixnumber);
+		break;
+	}
+	case 3: {//---------DANE--------PSEUDOLOSOWE------------
+		enterMartixNumberFromRandom(matrixarray, matrixspec, matrixnumber);
+		cout << "Udało się";
+		break;
+	}
+	default:
+		return 0;
+	}
+	operationMenu(matrixarray, matrixspec, matrixnumber);
+	return 1;
 }
-
-
-
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
--- DzialaniaNaMacierzach/funkcje.h
+++ DzialaniaNaMacierzach/funkcje.h
@@ -3,13 +3,11 @@
 #ifndef FUNKCJE_H
 #define FUNKCJE_H
 using namespace std;
-
-int enterMartixNumber();
 int startMenu();
+void operationMenu(int***, int**, int*);
 void enterMartixNumberFromKeyboard(int***, int**, int*);
 void enterMartixNumberFromRandom(int***, int**, int*);
 void enterMartixNumberFromFile(string,int***,int**,int*);
-bool walidacjaPliku(string);
- 
+bool walidacjaPliku(string); 
 #endif
  
